c417c98849683da62e4227f4187619779ec21d23
Erik Uus <erik.uus@gmail.com>
2023-01-25 16:00:23 +0200

    Update icon_component.ex

--- lib/live_playground_web/components/icon_component.ex
+++ lib/live_playground_web/components/icon_component.ex
@@ -82,5 +82,21 @@ defmodule LivePlaygroundWeb.IconComponent do
     """
   end
 
+  defp svg("clock", class) do
+    """
+    <svg class="#{class}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+    """
+  end
+
+  defp svg("paper_airplane", class) do
+    """
+    <svg class="#{class}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+    </svg>
+    """
+  end
+
   defp svg(_, _), do: nil
 end
