8df4b8dd62c84a53f4e45496cad8013af0148af1
Erik Uus <erik.uus@gmail.com>
2023-03-16 23:18:38 +0200

    Added initials

---
+++ lib/live_playground_web/live/init_live.ex
@@ -0,0 +1,35 @@
+defmodule LivePlaygroundWeb.InitLive do
+  use LivePlaygroundWeb, :live_view
+
+  def on_mount(:default, _params, _session, socket) do
+    socket =
+      attach_hook(socket, :set_current_path, :handle_params, fn
+        _params, url, socket ->
+          {:cont, assign(socket, page_title: page_title(URI.parse(url).path))}
+      end)
+
+    {:cont, socket}
+  end
+
+  defp page_title(path) do
+    Map.get(
+      %{
+        "/clicks" => "Clicks",
+        "/changes" => "Changes",
+        "/send-interval" => "Send",
+        "/autocomplete" => "Autocomplete",
+        "/search" => "Search",
+        "/filter" => "Filter",
+        "/modals" => "Modals",
+        "/upload" => "Upload"
+      },
+      path
+    )
+  end
+
+  def render(assigns) do
+    ~H"""
+
+    """
+  end
+end
--- lib/live_playground_web/router.ex
+++ lib/live_playground_web/router.ex
@@ -14,30 +14,32 @@ defmodule LivePlaygroundWeb.Router do
     plug :accepts, ["json"]
   end
 
+  live_session :default, on_mount: LivePlaygroundWeb.InitLive do
+    scope "/", LivePlaygroundWeb do
+      pipe_through :browser
+
+      live "/clicks", ClicksLive
+      live "/clicks/show-list", ClicksLive, :show_list
+      live "/changes", ChangesLive
+      live "/send-interval", SendIntervalLive
+      live "/send-after", SendAfterLive
+      live "/search", SearchLive
+      live "/search-advanced", SearchAdvancedLive
+      live "/autocomplete", AutocompleteLive
+      live "/autocomplete-advanced", AutocompleteAdvancedLive
+      live "/filter", FilterLive
+      live "/filter-advanced", FilterAdvancedLive
+      live "/modals", ModalsLive
+      live "/modals-advanced", ModalsAdvancedLive
+      live "/modals-advanced/text", ModalsAdvancedLive, :text
+      live "/modals-advanced/math", ModalsAdvancedLive, :math
+      live "/modals-advanced/image", ModalsAdvancedLive, :image
+      live "/upload", UploadLive
+    end
+  end
+
   scope "/", LivePlaygroundWeb do
     pipe_through :browser
-
-    live "/clicks", ClicksLive
-    live "/clicks/show-list", ClicksLive, :show_list
-    live "/changes", ChangesLive
-    live "/send-interval", SendIntervalLive
-    live "/send-after", SendAfterLive
-    live "/search", SearchLive
-    live "/search-advanced", SearchAdvancedLive
-    live "/autocomplete", AutocompleteLive
-    live "/autocomplete-advanced", AutocompleteAdvancedLive
-    live "/filter", FilterLive
-    live "/filter-advanced", FilterAdvancedLive
-
-    live "/modals", ModalsLive
-    # modals
-    live "/modals-advanced", ModalsAdvancedLive
-    live "/modals-advanced/text", ModalsAdvancedLive, :text
-    live "/modals-advanced/math", ModalsAdvancedLive, :math
-    live "/modals-advanced/image", ModalsAdvancedLive, :image
-    # endmodals
-
-    live "/upload", UploadLive
 
     get "/", PageController, :home
   end
