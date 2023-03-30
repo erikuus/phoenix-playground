90a243962d9eb5b2778dae76659c868a8c86e7d4
Erik Uus <erik.uus@gmail.com>
2023-03-30 20:37:49 +0300

    Add roundness to table rows

--- lib/live_playground_web/components/core_components.ex
+++ lib/live_playground_web/components/core_components.ex
@@ -549,7 +549,7 @@ defmodule LivePlaygroundWeb.CoreComponents do
     <table class="mt-11 w-full">
       <thead class="text-left text-[0.8125rem] leading-6 text-zinc-500">
         <tr>
-          <th :for={col <- @col} class={["p-0 pb-4 pr-6 font-normal", col[:class]]}>
+          <th :for={col <- @col} class={["p-0 pb-4 font-normal", col[:class]]}>
             <%= col[:label] %>
           </th>
           <th class="relative p-0 pb-4">
@@ -568,8 +568,9 @@ defmodule LivePlaygroundWeb.CoreComponents do
             phx-click={@row_click && @row_click.(row)}
             class={["relative p-0", col[:class], @row_click && "hover:cursor-pointer"]}
           >
-            <div class="block py-4 pr-6">
-              <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50" />
+            <div class="block py-4">
+              <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-lg" />
+              <span :if={@action == []} class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-lg" />
               <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
                 <%= render_slot(col, @row_item.(row)) %>
               </span>
@@ -577,7 +578,7 @@ defmodule LivePlaygroundWeb.CoreComponents do
           </td>
           <td :if={@action != []} class="relative p-0 w-14">
             <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
-              <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50" />
+              <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
               <span :for={action <- @action} class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700">
                 <%= render_slot(action, @row_item.(row)) %>
               </span>
