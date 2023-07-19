39379c5dfed6cebcf37937f3933a8f6d262226f9
Erik Uus <erik.uus@gmail.com>
2023-07-19 21:09:22 +0300

    Add ellipsis and github link to code snippets

--- lib/live_playground_web/helpers/demo_helpers.ex
+++ lib/live_playground_web/helpers/demo_helpers.ex
@@ -40,7 +40,9 @@ defmodule LivePlaygroundWeb.DemoHelpers do
         #{format_filename(filename)}
       </div>
       <div class="overflow-auto overscroll-auto bg-[#f8f8f8] px-4 py-5 sm:p-6">
+        <div class="text-lg text-gray-400 tracking-widest -mt-2 mb-3">...</div>
         #{read_file(filename) |> show_marked(from, to, elixir) |> hide_marked() |> Makeup.highlight()}
+        <div class="text-lg text-gray-400 tracking-widest mb-1">...</div>
       </div>
     </div>
     """
@@ -64,7 +66,16 @@ defmodule LivePlaygroundWeb.DemoHelpers do
     basename = Path.basename(filename)
 
     """
-    <span class="hidden md:inline">#{dirname}/</span>#{basename}
+    <div class="flex justify-between items-center">
+      <div>
+        <span class="hidden md:inline">#{dirname}/</span>#{basename}
+      </div>
+      <a target="_blank" href="https://github.com/erikuus/phoenix-playground/tree/main/#{dirname}/#{basename}">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+        </svg>
+      </a>
+    </div>
     """
   end
 
