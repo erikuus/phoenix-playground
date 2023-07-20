b9f78511f9b7b86d86bb52d22637fab4764ca0cd
Erik Uus <erik.uus@gmail.com>
2023-07-21 01:25:40 +0300

    Change map center

--- assets/js/hooks/map-dataset.js
+++ assets/js/hooks/map-dataset.js
@@ -2,7 +2,7 @@ import Map from "../leaflets/map-dataset.js"
 
 let MapDataset = {
   mounted() {
-    this.map = new Map(this.el, [58.668252, 25.048828], 7)
+    this.map = new Map(this.el, [58.6307, 25.3397], 7)
 
     JSON.parse(this.el.dataset.locations).forEach(location => {
       this.map.addMarker(location);
--- assets/js/hooks/map-push-events.js
+++ assets/js/hooks/map-push-events.js
@@ -2,7 +2,7 @@ import Map from "../leaflets/map-push-events.js"
 
 let MapPushEvents = {
   mounted() {
-    this.map = new Map(this.el, [58.668252, 25.048828], 7, event => {
+    this.map = new Map(this.el, [58.6307, 25.3397], 7, event => {
       locationId = event.target.options.locationId
       this.pushEvent("marker-clicked", locationId, (reply, ref) => {
         this.scrollTo(reply.location.id);
