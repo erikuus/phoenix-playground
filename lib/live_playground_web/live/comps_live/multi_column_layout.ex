800b0cda63180cd2afcd79cc834e3ab2e6bf8450
Erik Uus <erik.uus@gmail.com>
2023-09-27 12:56:02 +0300

    Finalize multi-column layout example

--- lib/live_playground_web/live/comps_live/multi_column_layout.ex
+++ lib/live_playground_web/live/comps_live/multi_column_layout.ex
@@ -20,8 +20,15 @@ defmodule LivePlaygroundWeb.CompsLive.MultiColumnLayout do
     </.header>
     <.resizable_iframe hook="IframeResize" id="multi-column-layout-demo" src="/multi-column-layout-demo" />
     <div class="mt-10 space-y-6">
-      <%= raw(code("lib/live_playground_web/live/comps_live/multi_column_layout_demo.ex")) %>
       <%= raw(code("lib/live_playground_web/components/layouts/demo.html.heex")) %>
+      <%= raw(code("lib/live_playground_web/live/comps_live/multi_column_layout_demo.ex")) %>
+      <.note icon="hero-information-circle">
+        Please note that this playground is itself constructed using a multi-column layout component, making it an excellent example of its usage.
+        Open any page and resize the window to observe how the layout responds. Also take a look at how the layout component is implemented in the
+        <.github_link class="underline" filename="lib/live_playground_web/components/layouts/recipes.html.heex">
+          source code
+        </.github_link>.
+      </.note>
     </div>
     """
   end
