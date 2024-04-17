50a7fae4aefdd3d79363fb9b63761b9bcf4c8cea
Erik Uus <erik.uus@gmail.com>
2024-04-17 18:31:47 +0300

    Update click_buttons.ex

--- lib/live_playground_web/live/recipes_live/click_buttons.ex
+++ lib/live_playground_web/live/recipes_live/click_buttons.ex
@@ -4,15 +4,13 @@ defmodule LivePlaygroundWeb.RecipesLive.ClickButtons do
   alias LivePlayground.Countries
 
   def mount(_params, _session, socket) do
-    countries = Countries.list_region_country("Baltic Countries")
+    case Countries.list_region_country("Baltic Countries") do
+      [] ->
+        {:ok, assign(socket, countries: [], selected_country: nil)}
 
-    socket =
-      assign(socket,
-        countries: countries,
-        selected_country: hd(countries)
-      )
-
-    {:ok, socket}
+      countries ->
+        {:ok, assign(socket, countries: countries, selected_country: hd(countries))}
+    end
   end
 
   def render(assigns) do
@@ -30,37 +28,12 @@ defmodule LivePlaygroundWeb.RecipesLive.ClickButtons do
         :for={country <- @countries}
         phx-click="select-country"
         phx-value-id={country.id}
-        type={get_button_type(country, @selected_country)}
+        kind={get_button_kind(country, @selected_country)}
       >
         <%= country.name %>
       </.button_link>
     </div>
-
-    <.list class="mt-6 mb-16 ml-1">
-      <:item title="Code"><%= @selected_country.code %></:item>
-
-      <:item title="Continent"><%= @selected_country.continent %></:item>
-
-      <:item title="Region"><%= @selected_country.region %></:item>
-
-      <:item title="The year of independence"><%= @selected_country.indepyear %></:item>
-
-      <:item title="The form of government"><%= @selected_country.governmentform %></:item>
-
-      <:item title="The head of state"><%= @selected_country.headofstate %></:item>
-
-      <:item title="Population">
-        <%= Number.Delimit.number_to_delimited(@selected_country.population, precision: 0, delimiter: " ") %>
-      </:item>
-
-      <:item title="GNP">
-        <%= Number.Delimit.number_to_delimited(@selected_country.gnp, precision: 0, delimiter: " ") %>
-      </:item>
-
-      <:item title="Life expectancy">
-        <%= Number.Delimit.number_to_delimited(@selected_country.lifeexpectancy, precision: 2) %>
-      </:item>
-    </.list>
+    <%= country_details(assigns) %>
     <!-- start hiding from live code -->
     <div class="mt-10 space-y-6">
       <.code_block filename="lib/live_playground_web/live/recipes_live/click_buttons.ex" />
@@ -70,12 +43,42 @@ defmodule LivePlaygroundWeb.RecipesLive.ClickButtons do
     """
   end
 
+  defp country_details(%{selected_country: nil} = assigns) do
+    ~H"""
+    <.alert kind={:info} close={false}>
+      No countries found in the specified region.
+    </.alert>
+    """
+  end
+
+  defp country_details(assigns) do
+    ~H"""
+    <.list class="mt-6 mb-16 ml-1">
+      <:item title="Code"><%= @selected_country.code %></:item>
+      <:item title="Continent"><%= @selected_country.continent %></:item>
+      <:item title="Region"><%= @selected_country.region %></:item>
+      <:item title="The year of independence"><%= @selected_country.indepyear %></:item>
+      <:item title="The form of government"><%= @selected_country.governmentform %></:item>
+      <:item title="The head of state"><%= @selected_country.headofstate %></:item>
+      <:item title="Population">
+        <%= Number.Delimit.number_to_delimited(@selected_country.population, precision: 0, delimiter: " ") %>
+      </:item>
+      <:item title="GNP">
+        <%= Number.Delimit.number_to_delimited(@selected_country.gnp, precision: 0, delimiter: " ") %>
+      </:item>
+      <:item title="Life expectancy">
+        <%= Number.Delimit.number_to_delimited(@selected_country.lifeexpectancy, precision: 2) %>
+      </:item>
+    </.list>
+    """
+  end
+
   def handle_event("select-country", %{"id" => id}, socket) do
     socket = assign(socket, :selected_country, Countries.get_country!(id))
     {:noreply, socket}
   end
 
-  def get_button_type(country, selected_country) do
-    if country == selected_country, do: "primary", else: "secondary"
+  def get_button_kind(country, selected_country) do
+    if country == selected_country, do: :primary, else: :secondary
   end
 end
