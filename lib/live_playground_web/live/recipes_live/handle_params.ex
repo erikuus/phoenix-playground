e6076395f26707e7514aa0c98f52c0c0ac7dc411
Erik Uus <erik.uus@gmail.com>
2024-04-22 13:16:15 +0300

    Update handle_params.ex

--- lib/live_playground_web/live/recipes_live/handle_params.ex
+++ lib/live_playground_web/live/recipes_live/handle_params.ex
@@ -4,22 +4,29 @@ defmodule LivePlaygroundWeb.RecipesLive.HandleParams do
   alias LivePlayground.Countries
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(socket,
-        countries: Countries.list_region_country("Baltic Countries")
-      )
+    countries = Countries.list_region_country("Baltic Countries")
 
-    {:ok, socket}
+    {:ok, assign(socket, :countries, countries)}
   end
 
   def handle_params(%{"id" => id}, _url, socket) do
-    country = Countries.get_country!(id)
-    {:noreply, assign(socket, :selected_country, country)}
+    try do
+      country = Countries.get_country!(id)
+      {:noreply, assign(socket, :selected_country, country)}
+    rescue
+      Ecto.NoResultsError ->
+        {:noreply, assign(socket, :selected_country, nil)}
+    end
   end
 
   def handle_params(_params, _url, socket) do
-    country = hd(socket.assigns.countries)
-    {:noreply, assign(socket, :selected_country, country)}
+    case socket.assigns.countries do
+      [] ->
+        {:noreply, assign(socket, :selected_country, nil)}
+
+      countries ->
+        {:noreply, assign(socket, :selected_country, hd(countries))}
+    end
   end
 
   def render(assigns) do
@@ -30,14 +37,46 @@ defmodule LivePlaygroundWeb.RecipesLive.HandleParams do
       <:subtitle>
         Managing URL Parameters in LiveView
       </:subtitle>
+      <:actions>
+        <.code_breakdown_link />
+      </:actions>
     </.header>
     <!-- end hiding from live code -->
-    <.tabs :if={@countries != []}>
+    <.tabs :if={@countries != []} class="mb-6">
       <:tab :for={country <- @countries} path={~p"/handle-params?#{[id: country.id]}"} active={country == @selected_country}>
         <%= country.name %>
       </:tab>
     </.tabs>
-    <.list class="mt-6 mb-16">
+    <%= country_details(assigns) %>
+    <!-- start hiding from live code -->
+    <div class="mt-10 space-y-6">
+      <.code_block filename="lib/live_playground_web/live/recipes_live/handle_params.ex" />
+      <.code_block filename="lib/live_playground/countries.ex" from="# handleparams" to="# endhandleparams" />
+    </div>
+    <.code_breakdown_slideover filename="priv/static/html/handle_params.html" />
+    <!-- end hiding from live code -->
+    """
+  end
+
+  defp country_details(%{countries: []} = assigns) do
+    ~H"""
+    <.alert kind={:info} close={false}>
+      No countries found in the specified region.
+    </.alert>
+    """
+  end
+
+  defp country_details(%{selected_country: nil} = assigns) do
+    ~H"""
+    <.alert kind={:info} close={false}>
+      No country found for the provided ID.
+    </.alert>
+    """
+  end
+
+  defp country_details(assigns) do
+    ~H"""
+    <.list class="mb-16 ml-1">
       <:item title="Code"><%= @selected_country.code %></:item>
       <:item title="Continent"><%= @selected_country.continent %></:item>
       <:item title="Region"><%= @selected_country.region %></:item>
@@ -54,12 +93,6 @@ defmodule LivePlaygroundWeb.RecipesLive.HandleParams do
         <%= Number.Delimit.number_to_delimited(@selected_country.lifeexpectancy, precision: 2) %>
       </:item>
     </.list>
-    <!-- start hiding from live code -->
-    <div class="mt-10 space-y-6">
-      <.code_block filename="lib/live_playground_web/live/recipes_live/handle_params.ex" />
-      <.code_block filename="lib/live_playground/countries.ex" from="# handleparams" to="# endhandleparams" />
-    </div>
-    <!-- end hiding from live code -->
     """
   end
 end
