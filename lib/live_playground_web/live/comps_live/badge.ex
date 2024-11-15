3b0bf287e78c8ad1e6dfb8f03bddc70a8a80e610
Erik Uus <erik.uus@gmail.com>
2024-11-15 17:50:35 +0200

    Update badge.ex

--- lib/live_playground_web/live/comps_live/badge.ex
+++ lib/live_playground_web/live/comps_live/badge.ex
@@ -3,6 +3,7 @@ defmodule LivePlaygroundWeb.CompsLive.Badge do
 
   def render(assigns) do
     ~H"""
+    <!-- start hiding from live code -->
     <.header class="mb-6">
       Badge
       <:subtitle>
@@ -14,6 +15,7 @@ defmodule LivePlaygroundWeb.CompsLive.Badge do
         </.goto_definition>
       </:actions>
     </.header>
+    <!-- end hiding from live code -->
     <div class="flex items-start flex-col space-x-0 space-y-3 md:flex-row md:space-x-3 md:space-y-0">
       <.badge>Default</.badge>
       <.badge kind={:red}>Red</.badge>
@@ -25,9 +27,11 @@ defmodule LivePlaygroundWeb.CompsLive.Badge do
       <.badge kind={:pink}>Pink</.badge>
       <.badge class="text-sm font-bold px-4 py-2">Custom Style</.badge>
     </div>
+    <!-- start hiding from live code -->
     <div class="mt-10">
       <.code_block filename="lib/live_playground_web/live/comps_live/badge.ex" />
     </div>
+    <!-- end hiding from live code -->
     """
   end
 end
