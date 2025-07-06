acbd038a91abcbb392b34e7f22b1f8396b402905
Erik Uus <erik.uus@gmail.com>
2025-07-06 17:52:17 +0300

    Improve recipe

--- lib/live_playground_web/live/recipes_live/click_buttons.ex
+++ lib/live_playground_web/live/recipes_live/click_buttons.ex
@@ -33,10 +33,10 @@ defmodule LivePlaygroundWeb.RecipesLive.ClickButtons do
         phx-value-id={country.id}
         kind={get_button_kind(country, @selected_country)}
       >
-        <%= country.name %>
+        {country.name}
       </.button_link>
     </div>
-    <%= country_details(assigns) %>
+    <.country_details selected_country={@selected_country} />
     <!-- start hiding from live code -->
     <div class="mt-10 space-y-6">
       <.code_block filename="lib/live_playground_web/live/recipes_live/click_buttons.ex" />
@@ -58,20 +58,20 @@ defmodule LivePlaygroundWeb.RecipesLive.ClickButtons do
   defp country_details(assigns) do
     ~H"""
     <.list class="mt-6 mb-16 ml-1">
-      <:item title="Code"><%= @selected_country.code %></:item>
-      <:item title="Continent"><%= @selected_country.continent %></:item>
-      <:item title="Region"><%= @selected_country.region %></:item>
-      <:item title="The year of independence"><%= @selected_country.indepyear %></:item>
-      <:item title="The form of government"><%= @selected_country.governmentform %></:item>
-      <:item title="The head of state"><%= @selected_country.headofstate %></:item>
+      <:item title="Code">{@selected_country.code}</:item>
+      <:item title="Continent">{@selected_country.continent}</:item>
+      <:item title="Region">{@selected_country.region}</:item>
+      <:item title="The year of independence">{@selected_country.indepyear}</:item>
+      <:item title="The form of government">{@selected_country.governmentform}</:item>
+      <:item title="The head of state">{@selected_country.headofstate}</:item>
       <:item title="Population">
-        <%= Number.Delimit.number_to_delimited(@selected_country.population, precision: 0, delimiter: " ") %>
+        {Number.Delimit.number_to_delimited(@selected_country.population, precision: 0, delimiter: " ")}
       </:item>
       <:item title="GNP">
-        <%= Number.Delimit.number_to_delimited(@selected_country.gnp, precision: 0, delimiter: " ") %>
+        {Number.Delimit.number_to_delimited(@selected_country.gnp, precision: 0, delimiter: " ")}
       </:item>
       <:item title="Life expectancy">
-        <%= Number.Delimit.number_to_delimited(@selected_country.lifeexpectancy, precision: 2) %>
+        {Number.Delimit.number_to_delimited(@selected_country.lifeexpectancy, precision: 2)}
       </:item>
     </.list>
     """
@@ -82,7 +82,7 @@ defmodule LivePlaygroundWeb.RecipesLive.ClickButtons do
     {:noreply, socket}
   end
 
-  def get_button_kind(country, selected_country) do
+  defp get_button_kind(country, selected_country) do
     if country == selected_country, do: :primary, else: :secondary
   end
 end
