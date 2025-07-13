14d4111f2f5a7f2cc6ab1f4ed6bd04b89c1688c0
Erik Uus <erik.uus@gmail.com>
2025-07-13 09:54:39 +0300

    Refactor StreamInsert LiveView and update docs
    Reorganized the StreamInsert LiveView to improve form initialization and reset logic by introducing a dedicated get_empty_form/0 helper. Updated the template to use curly brace interpolation for city fields and added autocomplete="off" to form inputs. Enhanced the HTML documentation to clarify the use of custom DOM IDs, form handling, selective streaming updates, and the performance benefits of LiveView streams.

--- lib/live_playground_web/live/recipes_live/stream_insert.ex
+++ lib/live_playground_web/live/recipes_live/stream_insert.ex
@@ -7,11 +7,17 @@ defmodule LivePlaygroundWeb.RecipesLive.StreamInsert do
   def mount(_params, _session, socket) do
     socket =
       socket
+      |> assign(:form, get_empty_form())
       |> stream_configure(:cities, dom_id: &"city-#{&1.countrycode}-#{&1.id}")
       |> stream(:cities, Cities.list_country_city("EST"))
-      |> assign(:form, get_empty_form())
 
     {:ok, socket}
+  end
+
+  defp get_empty_form() do
+    %City{}
+    |> Cities.change_city()
+    |> to_form()
   end
 
   def render(assigns) do
@@ -28,28 +34,28 @@ defmodule LivePlaygroundWeb.RecipesLive.StreamInsert do
     </.header>
     <!-- end hiding from live code -->
     <.form for={@form} phx-submit="save" class="flex flex-col space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-      <.input field={@form[:name]} label="Name" class="flex-auto" />
-      <.input field={@form[:district]} label="District" class="flex-auto" />
-      <.input field={@form[:population]} label="Population" class="flex-auto" />
+      <.input field={@form[:name]} label="Name" class="flex-auto" autocomplete="off" />
+      <.input field={@form[:district]} label="District" class="flex-auto" autocomplete="off" />
+      <.input field={@form[:population]} label="Population" class="flex-auto" autocomplete="off" />
       <div>
         <.button phx-disable-with="" class="md:mt-8">Save</.button>
       </div>
     </.form>
     <.table id="cities" rows={@streams.cities}>
       <:col :let={{_dom_id, city}} label="Name">
-        <%= city.name %>
+        {city.name}
         <dl class="font-normal md:hidden">
           <dt class="sr-only">District</dt>
-          <dd class="mt-1 truncate text-zinc-700"><%= city.district %></dd>
+          <dd class="mt-1 truncate text-zinc-700">{city.district}</dd>
         </dl>
         <dl class="hidden md:block font-normal text-xs text-zinc-400">
           <dt>Stream inserted:</dt>
-          <dd><%= Timex.now() %></dd>
+          <dd>{Timex.now()}</dd>
         </dl>
       </:col>
-      <:col :let={{_dom_id, city}} label="District" class="hidden md:table-cell"><%= city.district %></:col>
+      <:col :let={{_dom_id, city}} label="District" class="hidden md:table-cell">{city.district}</:col>
       <:col :let={{_dom_id, city}} label="Population" class="text-right md:pr-10">
-        <%= Number.Delimit.number_to_delimited(city.population, precision: 0, delimiter: " ") %>
+        {Number.Delimit.number_to_delimited(city.population, precision: 0, delimiter: " ")}
       </:col>
       <:action :let={{dom_id, city}}>
         <.link phx-click={JS.push("delete", value: %{id: city.id}) |> hide("##{dom_id}")} data-confirm="Are you sure?">
@@ -75,14 +81,13 @@ defmodule LivePlaygroundWeb.RecipesLive.StreamInsert do
       {:ok, city} ->
         socket =
           socket
-          |> stream_insert(:cities, city, at: 0)
           |> assign(:form, get_empty_form())
+          |> stream_insert(:cities, city, at: 0)
 
         {:noreply, socket}
 
       {:error, changeset} ->
-        socket = assign(socket, :form, to_form(changeset))
-        {:noreply, socket}
+        {:noreply, assign(socket, :form, to_form(changeset))}
     end
   end
 
@@ -91,11 +96,5 @@ defmodule LivePlaygroundWeb.RecipesLive.StreamInsert do
     {:ok, _} = Cities.delete_city(city)
 
     {:noreply, stream_delete(socket, :cities, city)}
-  end
-
-  defp get_empty_form() do
-    %City{}
-    |> Cities.change_city()
-    |> to_form()
   end
 end
